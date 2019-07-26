11e21e87cc74af70ca277fe0c58ef79f138279b2
OreOS Lawiet <oreoslawiet@gmail.com>
2019-07-25 19:07:48 -0700

    Orden Item
    Fecha: 25 / 07 / 19
    Descripcion:
    Angular Material añadido
    Finalizacion de Orden-Item
    Creacion de HTML en Orden

--- FrontEnd/src/app/ordenes/orden-items/orden-items.component.ts
+++ FrontEnd/src/app/ordenes/orden-items/orden-items.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { OrdenItem } from 'src/app/shared/orden-item.model';
 
 @Component({
   selector: 'app-orden-items',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orden-items.component.css']
 })
 export class OrdenItemsComponent implements OnInit {
+  formData: OrdenItem;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data,
+    public dialogRef : MatDialogRef<OrdenItemsComponent>) { 
 
-  constructor() { }
+    }
 
   ngOnInit() {
+    this.formData = {
+      OrdenItemId: null,
+      OrdenId: this.data.OrdenID,
+      ItemId: 0, 
+      NombreItem: '',
+      Precio: 0,
+      Cantidad: 0,
+      Total:0
+    }
   }
 
 }
