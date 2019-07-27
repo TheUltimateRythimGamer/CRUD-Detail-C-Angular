64005aaf4080fe3fa542e2a8f792747d138c5b72
OreOS Lawiet <oreoslawiet@gmail.com>
2019-07-27 11:46:46 -0700

    I will do this commit before i shit someting today
    Fecha: 27/07/19
    Descripcion:

--- FrontEnd/src/app/ordenes/orden-items/orden-items.component.ts
+++ FrontEnd/src/app/ordenes/orden-items/orden-items.component.ts
@@ -2,25 +2,24 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { OrdenItem } from 'src/app/shared/orden-item.model';
 import { ItemService } from 'src/app/shared/item.service';
+import { Item } from 'src/app/shared/item.model';
 
 @Component({
   selector: 'app-orden-items',
   templateUrl: './orden-items.component.html',
-  styleUrls: ['./orden-items.component.css']
+  styleUrls: []
 })
 export class OrdenItemsComponent implements OnInit {
   formData: OrdenItem;
+  itemList: Item [];
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
     public dialogRef : MatDialogRef<OrdenItemsComponent>,
-    private _Item:ItemService) { 
-
+    private _Item:ItemService) {
     }
 
   ngOnInit() {
-    this._Item.getItemList().then(
-      res => {console.log(res)}
-    );
+    this._Item.getItemList().then( res => this.itemList = res as Item[]);
     this.formData = {
       OrdenItemId: null,
       OrdenId: this.data.OrdenID,
@@ -31,5 +30,16 @@ export class OrdenItemsComponent implements OnInit {
       Total:0
     }
   }
+  updatePrecio(ctrl){
+    if(ctrl.selectedIndex == 0){      
+      this.formData.Precio = 0;
+    }
+    else {
+      this.formData.Precio = this.itemList[ctrl.selectedIndex-1].Precio;
+      console.log(this.formData.Precio);
+      
+    }
+  }
+  
 
 }
